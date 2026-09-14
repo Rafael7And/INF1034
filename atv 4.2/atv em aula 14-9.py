298fc8981824ea09bc0411f3e8ec2fdf293e0ad7
Rafael Aula <rafaelc.pereira.12@gmail.com>
2026-09-14 12:12:44 -0300

    2

--- atv 4.2/atv em aula 14-9.py
+++ atv 4.2/atv em aula 14-9.py
@@ -1,4 +1,6 @@
 from turtle import *
+from random import randint
+
 
 def soma_10(x):
     return x + 10
@@ -6,25 +8,42 @@ def soma_10(x):
 t = Turtle()
 t.speed(0)
 
-# Plano cartesiano
+#plano cartesiano
+def plano_cartesiano():
+    t.color ('black')
+    # t.heading (0)
+    t.pu()
+    t.goto(-400,0)
+    t.pd()
+    t.goto(400,0)
+    t.stamp()
 
-# Eixo dos X
+    t.pu()
+    t.goto(0,-400)
+    t.pd()
+    t.goto(0,400)
+    t.lt(90)
+    t.stamp()
+    t.rt(90)
+plano_cartesiano()
+
+t.color("red")
+# t.pu()
+# t.goto(-100, soma_10(-100))
+# t.pd()
+# t.goto(100, soma_10(100))
+
+# O range vai do primeiro valor até o último -1
+# print(list(range(-100, 100)))
+
+# for x in range(-100, 100):
+#     print(x)
+
+t.color("blue")
 t.pu()
-t.goto(-300, 0)
+t.goto(-200, soma_10(-200))
 t.pd()
-t.goto(300, 0)
-t.stamp()
-
-# Eixo dos Y
-t.pu()
-t.goto(0, -300)
-t.pd()
-t.goto(0, 300)
-t.lt(90)
-t.stamp()
-
-
-
-
+for x in range(-99, 101):
+    t.goto(2 * x, soma_10(2*x))
 
 mainloop()
