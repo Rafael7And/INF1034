c36578adc6e431c05caf543f88676d40cd0d3d65
Rafael Aula <rafaelc.pereira.12@gmail.com>
2026-09-14 12:32:59 -0300

    3

--- atv 4.2/atv em aula 14-9.py
+++ atv 4.2/atv em aula 14-9.py
@@ -1,5 +1,6 @@
 from turtle import *
-from random import randint
+from time import sleep
+#site para ver o grafico https://www.geogebra.org/calculator
 
 
 def soma_10(x):
@@ -11,7 +12,6 @@ t.speed(0)
 #plano cartesiano
 def plano_cartesiano():
     t.color ('black')
-    # t.heading (0)
     t.pu()
     t.goto(-400,0)
     t.pd()
@@ -27,17 +27,19 @@ def plano_cartesiano():
     t.rt(90)
 plano_cartesiano()
 
+######
+
 t.color("red")
-# t.pu()
-# t.goto(-100, soma_10(-100))
-# t.pd()
-# t.goto(100, soma_10(100))
+t.pu()
+t.goto(-100, soma_10(-100))
+t.pd()
+t.goto(100, soma_10(100))
 
 # O range vai do primeiro valor até o último -1
-# print(list(range(-100, 100)))
+print(list(range(-100, 100)))
 
-# for x in range(-100, 100):
-#     print(x)
+for x in range(-100, 100):
+    print(x)
 
 t.color("blue")
 t.pu()
@@ -45,5 +47,9 @@ t.goto(-200, soma_10(-200))
 t.pd()
 for x in range(-99, 101):
     t.goto(2 * x, soma_10(2*x))
+
+sleep()
+
+
 
 mainloop()
