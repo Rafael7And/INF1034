c6f76b0d5ec4e5b575a91e80ba60aeee66f46657
rafael andrade -cs <rafaelc.pereira.12@gmail.com>
2026-09-14 23:16:33 -0300

    il

--- atv 4.2/atv em aula 14-9.py
+++ atv 4.2/atv em aula 14-9.py
@@ -3,8 +3,8 @@ from time import sleep
 #site para ver o grafico https://www.geogebra.org/calculator
 
 
-def soma_10(x):
-    return x + 10
+def raiz(x):
+    return x**0.5
 
 t = Turtle()
 t.speed(0)
@@ -29,26 +29,138 @@ plano_cartesiano()
 
 ######
 
-t.color("red")
-t.pu()
-t.goto(-100, soma_10(-100))
-t.pd()
-t.goto(100, soma_10(100))
+# t.color("red")
+# t.pu()
+# t.goto(-100, soma_10(-100))
+# t.pd()
+# t.goto(100, soma_10(100))
 
-# O range vai do primeiro valor até o último -1
-print(list(range(-100, 100)))
+# # O range vai do primeiro valor até o último -1
+# print(list(range(-100, 100)))
 
-for x in range(-100, 100):
-    print(x)
+# for x in range(-100, 100):
+#     print(x)
 
+# t.color("blue")
+# t.pu()
+# t.goto(-200, soma_10(-200))
+# t.pd()
+# for x in range(-99, 101):
+#     t.goto(2 * x, soma_10(2*x))
+
+
+###############  - y = √x
+
+plano_cartesiano()
 t.color("blue")
 t.pu()
-t.goto(-200, soma_10(-200))
+t.goto(0, raiz(0)*20)
 t.pd()
-for x in range(-99, 101):
-    t.goto(2 * x, soma_10(2*x))
 
-sleep()
+for x in range(1, 101):
+    t.goto(x*2, raiz(x)*20)
+
+sleep(3)
+t.clear()
+# ---------------- y = 1/x -************
+def inversa(x):
+    return 1/x
+
+plano_cartesiano()
+t.color("red")
+
+# Parte negativa
+t.pu()
+t.goto(-200, inversa(-100)*100)
+t.pd()
+
+for x in range(-99, 0):
+    t.goto(x*2, inversa(x)*100)
+
+# Parte positiva
+t.pu()
+t.goto(2, inversa(1)*100)
+t.pd()
+
+for x in range(1, 101):
+    t.goto(x*2, inversa(x)*100)
+
+sleep(3)
+t.clear()
+# ---------------- y = 2^x #############################
+
+def potencia(x):
+    return 2**x
+
+plano_cartesiano()
+t.color("red")
+t.pu()
+t.goto(-20, potencia(-10))
+t.pd()
+
+for x in range(-9, 9):
+    t.goto(x*20, potencia(x))
+
+sleep(3)
+t.clear()
+
+# ---------------- y = 5 - x² ############################
+def parabola1(x):
+    return 5 - x**2
+
+plano_cartesiano()
+t.color("red")
+t.pu()
+t.goto(-200, parabola1(-10)*20)
+t.pd()
+
+for x in range(-9, 11):
+    t.goto(x*20, parabola1(x)*20)
+
+sleep(3)
+t.clear()
+
+
+# ---------------- y = x² - 5x + 6 ----------------
+
+def parabola2(x):
+    return x**2 - 5*x + 6
+
+plano_cartesiano()
+t.color("red")
+
+t.pu()
+t.goto(-100, parabola2(-5)*10)
+t.pd()
+
+for x in range(-4,11):
+    t.goto(x*20, parabola2(x)*10)
+
+sleep(3)
+t.clear()
+
+# ---------------- y = x³ - x² - x + 1 -#####################
+def cubica(x):
+    return x**3 - x**2 - x + 1
+
+plano_cartesiano()
+t.color("red")
+t.pu()
+t.goto(-100, cubica(-5))
+t.pd()
+
+for x in range(-4, 6):
+    t.goto(x*20, cubica(x))
+
+sleep(3)
+t.clear()
+
+
+
+
+
+
+
 
 
 
